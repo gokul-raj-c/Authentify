1f5709e4879c38385c6beaf57a1523c13fff1f4b
Basith M Rasak <basithmrasak1@gmail.com>
2024-03-17 15:01:45 +0530

    Added validation

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from keras.models import load_model
 from PIL import Image
 import numpy as np
 from skimage import transform
+from mtcnn import MTCNN
+import cv2
 
 rslt = None
 file = None
@@ -13,7 +15,8 @@ app = Flask(__name__)
 
 UPLOAD_FOLDER = "static/uploads"
 
-model = load_model("model/densenet.h5") 
+model = load_model("model\deepfake_resnet50.h5") 
+detector = MTCNN()
 
 @app.route("/")
 def index():
@@ -25,7 +28,7 @@ def upload():
 
 @app.route("/predict", methods=["POST","GET"])
 def predict():
-    global rslt, file, model
+    global rslt, file, model, detector
     if request.method == "POST":
         class_label = ["Fake","Real"]
         f = request.files['file']
@@ -36,12 +39,26 @@ def predict():
 
         file = filename
 
-        image = load(pt) 
-        pred = model.predict(image)
-        print(pred)
-        c = np.argmax(pred)
-        rslt = class_label[c]
-        return "res"
+        image = cv2.imread(pt)
+        
+        faces = detector.detect_faces(image)
+        
+        if faces:
+            face = faces[0] 
+            x, y, w, h = face['box']
+            face_image = image[y:y+h, x:x+w]
+            face_image = cv2.resize(face_image, (128, 128))  
+            face_image = cv2.cvtColor(face_image, cv2.COLOR_BGR2RGB)
+            face_image = np.array(face_image).astype('float32') / 255
+            face_image = np.expand_dims(face_image, axis=0)
+
+            pred = model.predict(face_image)
+            c = np.argmax(pred)
+            rslt = class_label[c]
+            return "done"
+        else:
+            rslt = -1
+            return "no face"
     else:
         return render_template("index.html")
 
@@ -55,11 +72,3 @@ def result():
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('404.html'), 404
-
-
-def load(filename):
-    np_image = Image.open(filename)
-    np_image = np.array(np_image).astype('float32')/255
-    np_image = transform.resize(np_image, (128, 128, 3))
-    np_image = np.expand_dims(np_image, axis=0)
-    return np_image 
